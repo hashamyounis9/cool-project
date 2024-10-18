3ed57b7894dad32c77875165bbc275a8f5e411be
Hasham Younas <hashamyounis9@gmail.com>
2024-10-19 03:06:07 +0500

    Create app.cpp

---
+++ app.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+  int n1, n2;
+  n1 = std::cin>>n1;
+  n2 = std::cin>>n2;
+  int sum = n1 + n2;
+  std::cout << "Sum is: " << sum << std::endl;
+  return 0; 
+}
