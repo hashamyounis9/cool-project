67b7f4e45fe2d7e83c752c65c41ffb3efa8ceec6
Hasham Younas <hashamyounis9@gmail.com>
2024-10-28 09:33:57 +0500

    Structure into function

--- app.cpp
+++ app.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 using std::cout;
 using std::cin;
+using std::endl;
+
+int sum(int n1, int n2)
+{
+  return n1 + n2;
+}
 
 int main()
 {
   int n1, n2;
   cin >> n1 >> n2;
-  int sum = n1 + n2;
-  cout << "Sum is: " << sum <<endl;
+  int c = sum(n1, n2);
+  cout << "Sum is: " << c << endl;
   return 0; 
 }
